1c185105464b1b69f8c6f9139b8d39d2fb5f4bd5
tcheukueppo <tcheukueppo@tutanota.com>
2022-09-18 21:36:51 +0100

    done with task-1 of ch-182

--- challenge-182/perl/ch-1.pl
+++ challenge-182/perl/ch-1.pl
@@ -2,19 +2,36 @@
 
 use strict;
 use warnings;
+use feature      qw(signatures);
+use experimental qw(for_list);
+use builtin      qw(indexed);
+no  warnings     qw(experimental::builtin);
 
 use Test::More;
 
-while ( my $line = <DATA> ) {
-	my @array = split m/, /, $line
+sub first_max ( $string ) {
+    my @array = split m/, |\n/, $string;
+    my ( $index, $biggest ) = each @array;
+
+    foreach my ( $i, $v ) ( indexed @array ) {
+        if ( $v > $biggest ) {
+            $index   = $i;
+            $biggest = $v;
+        }
+    }
+
+    return $index;
 }
 
-my $expected = [2, 4];
+my $biggest;
+push $biggest->@*, first_max $_ foreach <DATA>;
 
-is_deeply( $got, $expected, "");
+is_deeply( $biggest, [ 2, 1, 4, 4 ], "find the index of the first biggest number in the list" );
 
-done_testing(1);
+done_testing( 1 );
 
 __DATA__
-5, 2, 9, 1, 7, 6
+5, 2, 9, 1, 9, 6
+5, 10, 9, 1, 10, 6
 4, 2, 3, 1, 5, 0
+4, 2, 3, 1, 20, 20
