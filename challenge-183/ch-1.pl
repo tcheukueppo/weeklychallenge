5b9e64965f6625266c26a8fd84fff6a8ded2445d
tcheukueppo <tcheukueppo@tutanota.com>
2022-10-04 06:02:49 +0100

    challenge 185

--- challenge-183/ch-1.pl
+++ challenge-183/ch-1.pl
@@ -8,11 +8,11 @@ use feature      qw(say);
 use Test::More;
 
 my @uniq;
-my @list = ( [ 1, 2 ], [ 3, 4 ], [ 5, 6 ], [ 1, 2 ] );
+my @list = ( [ 9, 1 ], [ 3, 7 ], [ 2, 5 ], [ 2, 5 ] );
 
 # Man..... you gotta use `for(|each)'!!! :)
 map { my $aref = $_; push @uniq, $aref unless grep { @$aref ~~ @$_ } @uniq } @list;
 
-is_deeply( [@uniq], [ [ 1, 2 ], [ 3, 4, ], [ 5, 6 ] ] );
+is_deeply( [@uniq], [ [ 9, 1 ], [ 3, 7, ], [ 2, 5 ] ] );
 
 done_testing( 1 );
